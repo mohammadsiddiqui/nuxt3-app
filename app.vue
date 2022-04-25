ad2c49552de9e9490e3c1a7b8d1cd1eb5928e456
Mohammad Siddiqui <mohammadsiddiqui>
2022-04-25 22:27:24 +0100

    sec

--- app.vue
+++ app.vue
@@ -3,8 +3,10 @@
     <ul>
       <li><router-link to="/">Home</router-link></li>
       <li><router-link to="/about">About</router-link></li>
+      <li><router-link to="/contact">Contact</router-link></li>
     </ul>
-
-    <NuxtPage />
+    <div class="container">
+      <NuxtPage />
+    </div>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,15 @@
 <template>
-  <h1>Index page</h1>
+  <div class="content">
+    <h1>Home page</h1>
+
+    <div class="image" v-for="x in 50" :key="x">
+      <img :src="`https://picsum.photos/900/450?random=${x}`" loading="lazy" />
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.image {
+  min-height: 450px;
+}
+</style>
